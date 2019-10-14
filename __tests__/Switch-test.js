4841989751c0044d5d53147c4868993d13fd5c84
emilykwok <ekwokchoon@gmail.com>
2019-10-14 16:27:16 -0700

    Files Setup
    I hope it's okay DX The tests passed but idk what I'm doing hahaha

---
+++ __tests__/Switch-test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import Switch_Top from '../Emily/Switch';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<Switch_Top />);
+});
---
+++ comps/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Switch_Top from '../Emily/Switch';
+
+function Main(){
+  return(
+    <View>
+      <Switch_Top />
+    </View>
+  )
+}
+
+export default Main;
