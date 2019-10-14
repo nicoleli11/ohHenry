297c32212c9ecae1a6458be50cf26a571c6be618
ashlynnchen <ashkiwi0607@gmail.com>
2019-10-14 15:35:15 -0700

    first commit with files set up correctly and tested

---
+++ comps/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Picker1 from '../Ashlynn/Picker'
+
+function Main(){
+  return(
+      <View>
+        <Picker1 />
+      </View>
+
+    )
+}
+export default Main;
