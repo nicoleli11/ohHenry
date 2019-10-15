d81f3cfa6ec9d43692e598a7839c02ad44ed0c2b
ashlynnchen <ashkiwi0607@gmail.com>
2019-10-14 21:09:17 -0700

    updating picker.js, finally got it to show

--- Ashlynn/Picker.js
+++ Ashlynn/Picker.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import {View, Text, Picker} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Picker, StyleSheet} from 'react-native';
+
 
 function Picker1(){
+  state= {
+    language:'color1',
+  }
   return(
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text>(this.state.language)</Text>
+      <Picker
+        selectedValue={this.state.language}
+        style={{height: 50, width: 100}}
+        onValueChange={(itemValue, itemIndex) =>
+          this.setState({language: itemValue})
+        }>
+        <Picker.Item label="Color" value="color1" />
+        <Picker.Item label="Color2" value="color2" />
+        </Picker>
+
     </View>
 
-  )
-}
+  );
+  }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9FA8DA',
+  },
+});
+
+
 
 export default Picker1;
